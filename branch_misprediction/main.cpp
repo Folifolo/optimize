9af3c956f20c23e61562f7de9a6e2c2b1eaf9566
Viol <Folifolo@yandex.ru>
2018-12-02 17:14:38 +0300

    create

---
+++ branch_misprediction/main.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    const unsigned arraySize = 32768;
+	int* data = new int[arraySize];
+
+    for (unsigned c = 0; c < arraySize; ++c)
+        data[c] = std::rand() % 256;
+
+
+	if (argc > 1)
+		if(argv[1][0] == '0')
+			std::sort(data, data + arraySize);
+
+    long long sum = 0;
+
+    for (unsigned i = 0; i < 10000; ++i)
+    {
+        for (unsigned c = 0; c < arraySize-1; ++c)
+        {
+			if (data[c] >= 128)
+			{
+				if (data[c]%7 >= data[c+1]%7)
+					sum += data[c];
+			}
+        }
+    }
+    std::cout << "sum = " << sum << std::endl;
+}
